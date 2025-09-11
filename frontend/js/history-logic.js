6cc7a282c2bc5f1d561a17980cc30a478d39dffe
rasoolkhan1010 <rasoolkhan990880@gmail.com>
2025-09-11 21:25:52 +0530

    Update history-logic.js

--- frontend/js/history-logic.js
+++ frontend/js/history-logic.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- Web Worker to load data ---
   const historyWorker = new Worker("js/csv-worker.js");
-  historyWorker.postMessage({ url: "approved_suggestion.csv" });
+  historyWorker.postMessage({ url: "frontend/approved_suggestion.csv" });
 
   historyWorker.onmessage = function (event) {
     tableLoading.style.display = "none";
@@ -86,3 +86,4 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   }
 });
+
