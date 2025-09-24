6af5523e189ed4b61efed2807df7823746115a79
rasoolkhan1010 <rasoolkhan990880@gmail.com>
2025-09-24 17:00:01 -0500

    Update suggestions-logic.js

--- frontend/js/suggestions-logic.js
+++ frontend/js/suggestions-logic.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
     company: "company",
     Itmdesc: "Itmdesc",
     Cost: "cost",
-    "Total_Stock": "total_stock",
+    "Total_Stock": "Total_Stock",
     "30_days": "30_days",
     "W3": "W3",
     "Recommended Quntitty": "Recommended Quntitty",
@@ -574,6 +574,7 @@ document.addEventListener("DOMContentLoaded", () => {
     XLSX.writeFile(workbook, "suggestions_export.xlsx");
   }
 });
+
 
 
 
